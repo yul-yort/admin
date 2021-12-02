03c6710422aca121f71fcf6576787b6875257fa1
afdavletkulov <afdavletkulov@rttv.ru>
2021-12-02 22:10:51 +0300

    добавил таблицу для agencies

--- src/view/UI/pages/agencies/index.tsx
+++ src/view/UI/pages/agencies/index.tsx
@@ -1,9 +1,19 @@
 import React, { FC } from "react";
 import { observer } from "mobx-react-lite";
 import css from "./styles.module.scss";
+import AgencyListTable from "./components/Table";
+
+
+
+
+
 
 const AgencyList: FC = observer(() => {
-  return <div className={css.page}>Agencies</div>;
+  return (
+    <div className={css.page}>
+      <AgencyListTable/>
+    </div>
+  )
 });
 
 export default AgencyList;
