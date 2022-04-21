1172f5be827b32bdd5502a32dc4207ceb18270f6
afdavletkulov <afdavletkulov@rttv.ru>
2022-04-21 14:49:24 +0500

    модальное окно

---
+++ src/view/UI/pages/agency/components/DetailRoutes/RoutesCreateModal.tsx
@@ -0,0 +1,12 @@
+// import { FC } from "react";
+// import { Modal } from "src/view/UI/components/common/Modal";
+
+// export const RoutesCreateModal: FC () => {
+//   return (
+//     <>
+//       // <Modal>
+      
+//       // </Modal>
+//     </>
+//   )
+// }
