482a9105dcdfb7690b3a250f98fce1e56a312b8d
gaisdav <gais.davletkildin@gmail.com>
2022-08-19 09:47:44 +0500

    fix merg

--- src/libs/mocks/data/localities/localitiesHandlers.ts
+++ src/libs/mocks/data/localities/localitiesHandlers.ts
@@ -38,32 +38,6 @@ export const localitiesHandlers = [
     );
   }),
 
-  rest.post<string>(EEndpoints.LOCALITY_CREATE, (req, res, ctx) => {
-    const body = JSON.parse(req.body);
-    localities.push({ ...body, id: uuid() });
-
-    return res(
-      ctx.json<ILocalityDTO[]>(localities),
-      ctx.delay(getTimeout()),
-      ctx.status(200)
-    );
-  }),
-
-  rest.post<string>(EEndpoints.LOCALITY_EDIT, (req, res, ctx) => {
-    const body = JSON.parse(req.body);
-    localities.forEach((agency, index) => {
-      if (agency.id === body.id) {
-        localities[index] = { ...agency, ...body };
-      }
-    });
-
-    return res(
-      ctx.json<ILocalityDTO[]>(localities),
-      ctx.delay(getTimeout()),
-      ctx.status(200)
-    );
-  }),
-
   rest.delete<string>(EEndpoints.LOCALITY_DELETE, (req, res, ctx) => {
     const id = JSON.parse(req.body);
     let deletedLocalityIndex: number | null = null;
