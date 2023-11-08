dacc86d98c29102a180c6f74cdb13d1167dd681c
afdavletkulov <afdavletkulov@rttv.ru>
2023-11-08 07:43:32 -0300

    add-react-phone

--- src/view/UI/components/shared/AgencyCreateEditForm/index.tsx
+++ src/view/UI/components/shared/AgencyCreateEditForm/index.tsx
@@ -7,11 +7,7 @@ import { useFieldArray, useFormContext } from "react-hook-form";
 import { getErrorText } from "src/libs/utils";
 import css from "./styles.module.scss";
 import { IAgencyCreateEditForm, ICreateOrEditAgencyFormFields } from "./types";
-import { isValidPhoneNumber } from "react-phone-number-input";
-import "react-phone-number-input/style.css";
-import PhoneInputWithCountry from "react-phone-number-input/react-hook-form";
-
-type PhoneFieldName = `phones.${number}.value`;
+import { PhoneInputHF } from "../../common/hook-form";
 
 export const AgencyCreateEditForm: FC<IAgencyCreateEditForm> = ({
   onSave,
@@ -29,12 +25,8 @@ export const AgencyCreateEditForm: FC<IAgencyCreateEditForm> = ({
     name: "phones",
   });
 
-  const test = (data: any) => {
-    console.log("data", data);
-  };
-
   return (
-    <form onSubmit={handleSubmit(test)}>
+    <form onSubmit={handleSubmit(onSave)}>
       <div className={css.row}>
         <TextField
           id="name"
@@ -56,21 +48,7 @@ export const AgencyCreateEditForm: FC<IAgencyCreateEditForm> = ({
       {fields.map((field, index) => {
         return (
           <div key={field.id} className={css.row}>
-            <PhoneInputWithCountry
-              name={`phones[${index}].value` as PhoneFieldName}
-              style={{
-                width: "100%",
-              }}
-              className={css.test}
-              defaultCountry="RU"
-              placeholder="Телефон"
-              international
-              countryCallingCodeEditable={false}
-              control={control}
-              rules={{
-                validate: (value = "") => isValidPhoneNumber(value),
-              }}
-            />
+            <PhoneInputHF control={control} name={`phones[${index}].value`} />
 
             <IconButton
               disabled={isSubmitting}
